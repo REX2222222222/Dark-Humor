14b9260fe3274d7f1384403f010b2a68e9f78da5
暐庭 王 <wangweiting418@gmail.com>
2024-11-23 15:58:38 +0800

    #

---
+++ 01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 黑色幽默
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+<<<<<<< HEAD
+            string str1 = "apple";
+            string str2 = "banana";
+
+            // 使用 String.Compare 方法比較字串
+            int result = String.Compare(str1, str2);
+
+            if (result < 0)
+            {
+                Console.WriteLine($"\"{str1}\" comes before \"{str2}\" in alphabetical order.");
+            }
+            else if (result > 0)
+            {
+                Console.WriteLine($"\"{str2}\" comes before \"{str1}\" in alphabetical order.");
+            }
+            else
+            {
+                Console.WriteLine($"\"{str1}\" and \"{str2}\" are equal in alphabetical order.");
+            }
+=======
+>>>>>>> dd3a55fcee9b1bc5909801da214bd92c4b2a4e49
+        }
+    }
+}
